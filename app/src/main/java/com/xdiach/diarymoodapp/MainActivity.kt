e1efe6e6dc033bfe9a2707ce203e9b6067cb2306
Mikhail Diachenko <xdiach00@stud.fit.vutbr.cz>
2023-04-11 21:59:07 +0200

    Add imports

--- app/src/main/java/com/xdiach/diarymoodapp/MainActivity.kt
+++ app/src/main/java/com/xdiach/diarymoodapp/MainActivity.kt
@@ -3,13 +3,6 @@ package com.xdiach.diarymoodapp
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.material3.MaterialTheme
-import androidx.compose.material3.Surface
-import androidx.compose.material3.Text
-import androidx.compose.runtime.Composable
-import androidx.compose.ui.Modifier
-import androidx.compose.ui.tooling.preview.Preview
 import com.xdiach.diarymoodapp.ui.theme.DiaryMoodAppTheme
 
 class MainActivity : ComponentActivity() {
@@ -17,24 +10,8 @@ class MainActivity : ComponentActivity() {
         super.onCreate(savedInstanceState)
         setContent {
             DiaryMoodAppTheme {
-                // A surface container using the 'background' color from the theme
-                Surface(modifier = Modifier.fillMaxSize(), color = MaterialTheme.colorScheme.background) {
-                    Greeting("Android")
-                }
+
             }
         }
-    }
-}
-
-@Composable
-fun Greeting(name: String) {
-    Text(text = "Hello $name!")
-}
-
-@Preview(showBackground = true)
-@Composable
-fun DefaultPreview() {
-    DiaryMoodAppTheme {
-        Greeting("Android")
     }
 }
