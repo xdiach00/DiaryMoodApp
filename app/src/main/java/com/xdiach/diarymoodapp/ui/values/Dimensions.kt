66cdf7e3f04febedaaff97328ad39c506226100c
Mikhail Diachenko <xdiach00@stud.fit.vutbr.cz>
2023-04-12 16:23:29 +0200

    Add object which contains most popular dimensions

---
+++ app/src/main/java/com/xdiach/diarymoodapp/ui/values/Dimensions.kt
@@ -0,0 +1,11 @@
+package com.xdiach.diarymoodapp.ui.values
+
+import androidx.compose.ui.unit.dp
+
+object Dimensions {
+    val XS = 2.dp
+    val S = 4.dp
+    val M = 8.dp
+    val L = 16.dp
+    val XL = 32.dp
+}
