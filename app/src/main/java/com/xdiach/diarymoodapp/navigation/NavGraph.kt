389f135adc00558cd77c7ca8d17d35fee7ded03e
Mikhail Diachenko <xdiach00@stud.fit.vutbr.cz>
2023-04-13 18:22:57 +0200

    Implement navigation drawer

--- app/src/main/java/com/xdiach/diarymoodapp/navigation/NavGraph.kt
+++ app/src/main/java/com/xdiach/diarymoodapp/navigation/NavGraph.kt
@@ -1,8 +1,11 @@
 package com.xdiach.diarymoodapp.navigation
 
-import androidx.compose.runtime.Composable
-import androidx.compose.runtime.getValue
+import androidx.compose.material3.DrawerValue
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.rememberDrawerState
+import androidx.compose.runtime.*
 import androidx.compose.ui.platform.LocalContext
+import androidx.compose.ui.res.stringResource
 import androidx.lifecycle.viewmodel.compose.viewModel
 import androidx.navigation.NavGraphBuilder
 import androidx.navigation.NavHostController
@@ -13,11 +16,17 @@ import androidx.navigation.navArgument
 import com.stevdzasan.messagebar.rememberMessageBarState
 import com.stevdzasan.onetap.rememberOneTapSignInState
 import com.xdiach.diarymoodapp.R
+import com.xdiach.diarymoodapp.presentation.components.DisplayAlertDialog
 import com.xdiach.diarymoodapp.presentation.screens.authentication.AuthenticationScreen
 import com.xdiach.diarymoodapp.presentation.screens.authentication.AuthenticationViewModel
 import com.xdiach.diarymoodapp.presentation.screens.home.HomeScreen
 import com.xdiach.diarymoodapp.ui.UiText
+import com.xdiach.diarymoodapp.util.Constants.APP_ID
 import com.xdiach.diarymoodapp.util.Constants.WRITE_SCREEN_ARGUMENT_KEY
+import io.realm.kotlin.mongodb.App
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.launch
+import kotlinx.coroutines.withContext
 
 @Composable
 fun SetupNavGraph(startDestination: String, navController: NavHostController) {
@@ -31,9 +40,15 @@ fun SetupNavGraph(startDestination: String, navController: NavHostController) {
                 navController.navigate(Screen.Home.route)
             }
         )
-        homeRoute(navigateToWrite = {
-            navController.navigate(Screen.Write.route)
-        })
+        homeRoute(
+            navigateToWrite = {
+                navController.navigate(Screen.Write.route)
+            },
+            navigateToAuth = {
+                navController.popBackStack()
+                navController.navigate(Screen.Authentication.route)
+            }
+        )
         writeRoute()
     }
 }
@@ -89,13 +104,44 @@ fun NavGraphBuilder.authenticationRoute(
     }
 }
 
+@OptIn(ExperimentalMaterial3Api::class)
 fun NavGraphBuilder.homeRoute(
     navigateToWrite: () -> Unit,
+    navigateToAuth: () -> Unit,
 ) {
     composable(route = Screen.Home.route) {
+        val drawerState = rememberDrawerState(initialValue = DrawerValue.Closed)
+        val scope = rememberCoroutineScope()
+        var signOutDialogOpened by remember { mutableStateOf(false) }
+
         HomeScreen(
-            onMenuClicked = {},
+            drawerState = drawerState,
+            onMenuClicked = {
+                scope.launch {
+                    drawerState.open()
+                }
+            },
+            onSignOutClicked = {
+                signOutDialogOpened = true
+            },
             navigateToWrite = navigateToWrite,
+        )
+        DisplayAlertDialog(
+            title = stringResource(id = R.string.home_screen_signout),
+            message = stringResource(id = R.string.home_screen_signout_description),
+            dialogOpened = signOutDialogOpened,
+            onCloseDialog = { signOutDialogOpened = false },
+            onYesClicked = {
+                scope.launch(Dispatchers.IO) {
+                    val user = App.Companion.create(APP_ID).currentUser
+                    if (user != null) {
+                        user.logOut()
+                        withContext(Dispatchers.Main) {
+                            navigateToAuth()
+                        }
+                    }
+                }
+            },
         )
     }
 }
--- app/src/main/java/com/xdiach/diarymoodapp/presentation/screens/home/HomeScreen.kt
+++ app/src/main/java/com/xdiach/diarymoodapp/presentation/screens/home/HomeScreen.kt
@@ -1,35 +1,95 @@
 package com.xdiach.diarymoodapp.presentation.screens.home
 
 import android.annotation.SuppressLint
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.*
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.Edit
-import androidx.compose.material3.ExperimentalMaterial3Api
-import androidx.compose.material3.FloatingActionButton
-import androidx.compose.material3.Icon
-import androidx.compose.material3.Scaffold
+import androidx.compose.material3.*
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.unit.dp
+import com.xdiach.diarymoodapp.R
 
 @OptIn(ExperimentalMaterial3Api::class)
 @SuppressLint("UnusedMaterial3ScaffoldPaddingParameter")
 @Composable
 fun HomeScreen(
+    drawerState: DrawerState,
     onMenuClicked: () -> Unit,
+    onSignOutClicked: () -> Unit,
     navigateToWrite: () -> Unit,
 ) {
-    Scaffold(
-        topBar = {
-            HomeTopBar(onMenuClicked = onMenuClicked)
-        },
-        floatingActionButton = {
-            FloatingActionButton(onClick = navigateToWrite) {
-                Icon(
-                    imageVector = Icons.Default.Edit,
-                    contentDescription = "New Note Icon"
-                )
-            }
-        },
-        content = {
+    NavigationDrawer(
+        drawerState = drawerState,
+        onSignOutClicked = onSignOutClicked
+    ) {
+        Scaffold(
+            topBar = {
+                HomeTopBar(onMenuClicked = onMenuClicked)
+            },
+            floatingActionButton = {
+                FloatingActionButton(onClick = navigateToWrite) {
+                    Icon(
+                        imageVector = Icons.Default.Edit,
+                        contentDescription = "New Note Icon"
+                    )
+                }
+            },
+            content = {
 
-        }
+            }
+        )
+    }
+}
+
+@OptIn(ExperimentalMaterial3Api::class)
+@Composable
+fun NavigationDrawer(
+    drawerState: DrawerState,
+    onSignOutClicked: () -> Unit,
+    content: @Composable () -> Unit,
+) {
+    ModalNavigationDrawer(
+        drawerState = drawerState,
+        drawerContent = {
+            ModalDrawerSheet(
+                content = {
+                    Box(
+                        modifier = Modifier
+                            .fillMaxWidth()
+                            .height(250.dp),
+                        contentAlignment = Alignment.Center,
+                    ) {
+                        Image(
+                            modifier = Modifier.size(250.dp),
+                            painter = painterResource(id = R.drawable.app_icon),
+                            contentDescription = stringResource(id = R.string.app_name),
+                        )
+                    }
+                    NavigationDrawerItem(
+                        label = {
+                            Row(modifier = Modifier.padding(horizontal = 12.dp)) {
+                                Image(
+                                    painter = painterResource(id = R.drawable.google_logo),
+                                    contentDescription = "Google logo"
+                                )
+                                Spacer(modifier = Modifier.width(12.dp))
+                                Text(
+                                    text = stringResource(id = R.string.home_screen_signout),
+                                    color = MaterialTheme.colorScheme.onSurface,
+                                )
+                            }
+                        },
+                        selected = false,
+                        onClick = onSignOutClicked,
+                    )
+                }
+            )
+        },
+        content = content,
     )
 }
