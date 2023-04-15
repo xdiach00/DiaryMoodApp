5efa6437c36966749fe26d928ab484802972c167
Mikhail Diachenko <xdiach00@stud.fit.vutbr.cz>
2023-04-15 23:19:59 +0200

    Add Home Screen View Model

---
+++ app/src/main/java/com/xdiach/diarymoodapp/presentation/screens/home/HomeViewModel.kt
@@ -0,0 +1,27 @@
+package com.xdiach.diarymoodapp.presentation.screens.home
+
+import androidx.compose.runtime.MutableState
+import androidx.compose.runtime.mutableStateOf
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import com.xdiach.diarymoodapp.data.repository.Diaries
+import com.xdiach.diarymoodapp.data.repository.MongoDB
+import com.xdiach.diarymoodapp.util.RequestState
+import kotlinx.coroutines.launch
+
+class HomeViewModel : ViewModel() {
+
+    var diaries: MutableState<Diaries> = mutableStateOf(RequestState.Idle)
+
+    init {
+        observeAllDiaries()
+    }
+
+    private fun observeAllDiaries() {
+        viewModelScope.launch {
+            MongoDB.getAllDiaries().collect { result ->
+                diaries.value = result
+            }
+        }
+    }
+}
