ad011ab5adb311f8e8415e7d1009919a83a3b88d
Mikhail Diachenko <xdiach00@stud.fit.vutbr.cz>
2023-04-19 20:04:03 +0200

    Add DAO and Database for images to upload

---
+++ app/src/main/java/com/xdiach/diarymoodapp/data/database/ImagesDatabase.kt
@@ -0,0 +1,14 @@
+package com.xdiach.diarymoodapp.data.database
+
+import androidx.room.Database
+import androidx.room.RoomDatabase
+import com.xdiach.diarymoodapp.data.database.entity.ImageToUpload
+
+@Database(
+    entities = [ImageToUpload::class],
+    version = 1,
+    exportSchema = false
+)
+abstract class ImagesDatabase: RoomDatabase() {
+    abstract fun imageToUploadDao(): ImagesToUploadDao
+}
---
+++ app/src/main/java/com/xdiach/diarymoodapp/di/DatabaseModule.kt
@@ -0,0 +1,33 @@
+package com.xdiach.diarymoodapp.di
+
+import android.content.Context
+import androidx.room.Room
+import com.xdiach.diarymoodapp.data.database.ImagesDatabase
+import com.xdiach.diarymoodapp.util.Constants.IMAGES_DATABASE
+import dagger.Module
+import dagger.Provides
+import dagger.hilt.InstallIn
+import dagger.hilt.android.qualifiers.ApplicationContext
+import dagger.hilt.components.SingletonComponent
+import javax.inject.Singleton
+
+@Module
+@InstallIn(SingletonComponent::class)
+object DatabaseModule {
+
+    @Provides
+    @Singleton
+    fun provideDatabase(
+        @ApplicationContext context: Context
+    ): ImagesDatabase {
+        return Room.databaseBuilder(
+            context = context,
+            klass = ImagesDatabase::class.java,
+            name = IMAGES_DATABASE
+        ).build()
+    }
+
+    @Provides
+    @Singleton
+    fun providesFirstDao(database: ImagesDatabase) = database.imageToUploadDao()
+}
