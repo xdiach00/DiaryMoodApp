05f30069b1ad81f11613c7d4bf72c6604068ae8f
Mikhail Diachenko <xdiach00@stud.fit.vutbr.cz>
2023-04-19 14:24:46 +0200

    Add function to fetch image from firebase

--- app/src/main/java/com/xdiach/diarymoodapp/util/UtilFunctions.kt
+++ app/src/main/java/com/xdiach/diarymoodapp/util/UtilFunctions.kt
@@ -2,8 +2,33 @@
 
 package com.xdiach.diarymoodapp.util
 
+import android.net.Uri
+import com.google.firebase.storage.FirebaseStorage
 import io.realm.kotlin.types.RealmInstant
 import java.time.Instant
+
+fun fetchImagesFromFirebase(
+    remoteImagePaths: List<String>,
+    onImageDownload: (Uri) -> Unit,
+    onImageDownloadFailed: (Exception) -> Unit = {},
+    onReadyToDisplay: () -> Unit = {}
+) {
+    if (remoteImagePaths.isNotEmpty()) {
+        remoteImagePaths.forEachIndexed { index, remoteImagePath ->
+            if (remoteImagePath.trim().isNotEmpty()) {
+                FirebaseStorage.getInstance().reference.child(remoteImagePath.trim()).downloadUrl
+                    .addOnSuccessListener {
+                        onImageDownload(it)
+                        if (remoteImagePath.lastIndexOf(remoteImagePath.last()) == index) {
+                            onReadyToDisplay()
+                        }
+                    }.addOnFailureListener {
+                        onImageDownloadFailed(it)
+                    }
+            }
+        }
+    }
+}
 
 fun RealmInstant.toInstant(): Instant {
     val sec: Long = this.epochSeconds
