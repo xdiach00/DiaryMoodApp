7f870cf6a85a24ffafde160f72b079c425b67173
Mikhail Diachenko <xdiach00@stud.fit.vutbr.cz>
2023-04-22 13:45:56 +0200

    Init a core:util module

---
+++ core/util/src/main/java/com/xdiach/util/Constants.kt
@@ -0,0 +1,13 @@
+package com.xdiach.diarymoodapp.util
+
+object Constants {
+
+    const val APP_ID = "diarymoodapp-ksrgj"
+    const val CLIENT_ID = "600652293047-qvc8savn0uapug2c9043drlt8ubhmsod.apps.googleusercontent.com"
+
+    const val WRITE_SCREEN_ARGUMENT_KEY = "diaryId"
+
+    const val IMAGES_DATABASE = "images_db"
+    const val IMAGE_TO_UPLOAD_TABLE = "image_to_upload_table"
+    const val IMAGE_TO_DELETE_TABLE = "image_to_delete_table"
+}
