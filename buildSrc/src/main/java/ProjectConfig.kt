f03d4bead2ce42d080f0492b834b0ebbc8ddd3e2
Mikhail Diachenko <xdiach00@stud.fit.vutbr.cz>
2023-05-17 22:26:37 +0200

    Update versionCode and versionName in project config

--- buildSrc/src/main/java/ProjectConfig.kt
+++ buildSrc/src/main/java/ProjectConfig.kt
@@ -3,8 +3,8 @@ object ProjectConfig {
     const val minSdk = 24
     const val targetSdk = 33
     const val composeExtensionVersion = "1.4.0"
-    const val versionCode = 1
-    const val versionName = "1.0"
+    const val versionCode = 2
+    const val versionName = "1.1"
     const val jvmTarget = "1.8"
     const val applicationId = "com.xdiach.diarymoodapp"
 
