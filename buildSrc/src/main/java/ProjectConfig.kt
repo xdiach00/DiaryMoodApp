9b34ecec6e3d7cf009e5fb60933e6174ecb43c5d
Mikhail Diachenko <xdiach00@stud.fit.vutbr.cz>
2023-06-18 14:34:38 +0200

    Revert versionCode to 1

--- buildSrc/src/main/java/ProjectConfig.kt
+++ buildSrc/src/main/java/ProjectConfig.kt
@@ -3,7 +3,7 @@ object ProjectConfig {
     const val minSdk = 24
     const val targetSdk = 33
     const val composeExtensionVersion = "1.4.0"
-    const val versionCode = 2
+    const val versionCode = 1
     const val versionName = "1.1"
     const val jvmTarget = "1.8"
     const val applicationId = "com.xdiach.diarymoodapp"
